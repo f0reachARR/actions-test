073a75bcb60c6d0b82cfeb438f345b37216cc2f6
f0reachARR <f0reach@f0reach.me>
2025-01-23 17:45:56 +0900

    test

---
+++ example.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+  const double frame_rate = 30;
+
+  const double frame = 30;
+
+  std::cout << std::boolalpha << ((frame / frame_rate) == 1.0) << std::endl;
+
+  return 0;
+}
